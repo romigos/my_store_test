653bb3e3e74aa03b77a1120273d6123f9d1ac971
romigos <bolist@mail.ru>
2019-06-15 00:11:41 +0300

    by brain

---
+++ db/migrate/20190614192046_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :price
+      t.text :description
+      t.integer :weight
+      t.boolean :real
+      t.timestamps
+    end
+  end
+end
