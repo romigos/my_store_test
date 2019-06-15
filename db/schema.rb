2f3d1cf65297523e3ad71be11a65441670375774
romigos <bolist@mail.ru>
2019-06-15 20:11:41 +0300

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,16 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_192046) do
-
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.text "description"
-    t.integer "weight"
-    t.boolean "real"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_190_614_192_046) do
+  create_table 'items', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'price'
+    t.text 'description'
+    t.integer 'weight'
+    t.boolean 'real'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
